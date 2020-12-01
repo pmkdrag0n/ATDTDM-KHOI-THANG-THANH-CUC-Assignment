7e4b129d70f72f0ba70aee0743a72ccd7f01eb12
BlackDrag0n <pmk.drag0n.1998@gmail.com>
2020-12-01 18:16:32 +0000

    fix xong loi, chay on dinh

--- app.py
+++ app.py
@@ -98,4 +98,4 @@ def create_items():
    return render_template('create_items.html', error=error)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=80)
+    app.run(host="0.0.0.0",port=5000)
